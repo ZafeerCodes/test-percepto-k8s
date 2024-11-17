92eb0992e2b3a5ce4fd68fe29d7a04c18e2163c6
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-11-18 02:46:59 +0530

    change

--- orchestrator/orchestrate.py
+++ orchestrator/orchestrate.py
@@ -12,7 +12,7 @@ try:
 except:
     config.load_kube_config()
 
-print("check tgus.........")
+print("check tgus........")
 kafkaInstance = KafkaPipe()
 producer = kafkaInstance.producer
 class Orchestrator:
